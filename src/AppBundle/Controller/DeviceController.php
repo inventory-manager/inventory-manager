d54e8ac0c5a223335c47f17478e0237b894f2d56
DerKaito <DerKaito99@gmail.com>
2015-12-20 23:39:30 +0100

    Erstellen des Device Controllers und anpassen des Device Models

---
+++ src/AppBundle/Controller/DeviceController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Models\Device;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Validator\Exception\ValidatorException;
+
+/**
+ * Class DeviceController
+ * @package AppBundle\Controller
+ */
+class DeviceController extends Controller
+{
+    /**
+     * @Route("/devices", name="get_all_device")
+     * @Method({"GET"})
+     * @return JsonResponse
+     */
+    public function getAllDevicesAction()
+    {
+        $devices = $this->get('device_manager')->getAllDevices();
+
+        return new JsonResponse($devices);
+    }
+
+    /**
+     * @Route("/devicestates", name="get_all_device_states")
+     * @Method({"GET"})
+     * @return JsonResponse
+     */
+    public function getAllDeviceStatesAction()
+    {
+        $deviceTypes = $this->get('device_manager')->getAllDeviceStates();
+
+        return new JsonResponse($deviceTypes);
+    }
+
+    /**
+     * @Route("/devices/{id}", name="get_single_device")
+     * @Method({"GET"})
+     * @param $id
+     * @return JsonResponse
+     */
+    public function getDeviceByIdAction($id)
+    {
+        /** @var Device $device */
+        $device = null;
+        try {
+            $device = $this->get('device_manager')->getDeviceById($id);
+        } catch (ResourceNotFoundException $e) {
+            throw $e;
+        }
+
+        return new JsonResponse($device);
+    }
+
+    /**
+     * @Route("/devices", name="update_device")
+     * @Method({"PUT"})
+     * @ParamConverter("updatedDevice", class="AppBundle:Device", converter="device_converter")
+     * @param Device $updatedDevice
+     * @return JsonResponse
+     */
+    public function updateDeviceAction(Device $updatedDevice)
+    {
+        $device = null;
+        try {
+            $device = $this->get('device_manager')->updateDevice($updatedDevice);
+        } catch (ValidatorException $e) {
+            throw $e;
+        }
+
+        return new JsonResponse($device, 200);
+    }
+
+    /**
+     * @Route("/devices/{id}", name="delete_device")
+     * @Method({"DELETE"})
+     * @ParamConverter("toBeDeletedDevice", class="AppBundle:Device")
+     * @param Device $toBeDeletedDevice
+     * @return JsonResponse
+     */
+    public function deleteDeviceAction(Device $toBeDeletedDevice)
+    {
+        $this->get('device_manager')->deleteDevice($toBeDeletedDevice);
+
+        return new JsonResponse($toBeDeletedDevice, 200);
+    }
+
+    /**
+     * @Route("/devices", name="create_device")
+     * @Method({"POST"})
+     * @ParamConverter("newDevice", class="AppBundle:Device", converter="device_converter")
+     * @param Device $newDevice
+     * @return JsonResponse
+     */
+    public function createDeviceAction(Device $newDevice)
+    {
+        try {
+            $this->get('device_manager')->createDevice($newDevice);
+        } catch (ValidatorException $e) {
+            throw $e;
+        }
+
+        return new JsonResponse($newDevice, 201);
+    }
+}
---
+++ src/AppBundle/Converter/DeviceConverter.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace AppBundle\Converter;
+
+use AppBundle\Models\Article;
+use AppBundle\Models\Device;
+use AppBundle\Models\DeviceState;
+use AppBundle\Models\Room;
+use Doctrine\ORM\EntityManager;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Sensio\Bundle\FrameworkExtraBundle\Request\ParamConverter\ParamConverterInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Class DeviceConverter
+ * @package AppBundle\Converter
+ */
+class DeviceConverter implements ParamConverterInterface
+{
+    /**
+     * @var EntityManager
+     */
+    private $entityManager;
+
+    /**
+     * DeviceConverter constructor.
+     * @param EntityManager $entityManager
+     */
+    public function __construct(EntityManager $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    public function apply(Request $request, ParamConverter $configuration)
+    {
+        $decoded = json_decode($request->getContent(), true);
+
+        if (!isset($decoded['id']) || !isset($decoded['serialNumber']) || !isset($decoded['inventoryNumber']) ||
+            !isset($decoded['buyDate']) || !isset($decoded['dueDate']) || !isset($decoded['inUse']) ||
+            !isset($decoded['comment']) || !isset($decoded['state']) || !isset($decoded['room']) ||
+            !isset($decoded['article'])) {
+            throw new \InvalidArgumentException('Device konnte nicht erstellt werden, fehlende Parameter', 400);
+        }
+
+        $device = Device::createDevice($decoded['id'], $decoded['serialNumber'], $decoded['inventoryNumber'],
+            $decoded['buyDate'], $decoded['dueDate'], $decoded['inUse'], $decoded['comment']);
+
+        /** @var DeviceState $state */
+        $state = $this->entityManager->find('AppBundle:DeviceState', $decoded['state']);
+        if ($state !== null) {
+            $device->setDeviceState($state);
+        } else {
+            throw new \InvalidArgumentException('Devicestate ' . $decoded['state'] . ' konnte nicht gefunden werden',
+                400);
+        }
+
+        /** @var Room $room */
+        $room = $this->entityManager->find('AppBundle:Room', $decoded['room']);
+        if ($room !== null) {
+            $device->setRoom($room);
+        } else {
+            throw new \InvalidArgumentException('Room ' . $decoded['room'] . ' konnte nicht gefunden werden', 400);
+        }
+
+        /** @var Article $article */
+        $article = $this->entityManager->find('AppBundle:Article', $decoded['article']);
+        if ($room !== null) {
+            $device->setArticle($article);
+        } else {
+            throw new \InvalidArgumentException('Article ' . $decoded['article'] . ' konnte nicht gefunden werden', 400);
+        }
+
+        $request->attributes->set($configuration->getName(), $device);
+
+        return true;
+    }
+
+    public function supports(ParamConverter $configuration)
+    {
+        return $configuration->getClass() === 'AppBundle:Device';
+    }
+}
