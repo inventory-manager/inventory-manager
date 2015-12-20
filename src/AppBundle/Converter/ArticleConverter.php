41fe60a7eb6c42d7b0174714a298338454dcddbf
haenschenklein <jon1993@gmx.de>
2015-12-20 21:38:28 +0100

    Article Contoller, Manager, Converter, Fixtures und Eintrag in Services.yaml

---
+++ src/AppBundle/Converter/ArticleConverter.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace AppBundle\Converter;
+
+use AppBundle\Models\Article;
+use AppBundle\Models\ArticleCategory;
+use AppBundle\Models\User;
+use Doctrine\ORM\EntityManager;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Sensio\Bundle\FrameworkExtraBundle\Request\ParamConverter\ParamConverterInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Class ArticleConverter
+ * @package AppBundle\Converter
+ */
+class ArticleConverter implements ParamConverterInterface
+{
+    /**
+     * @var EntityManager
+     */
+    private $entityManager;
+
+    /**
+     * ArticleConverter constructor.
+     * @param EntityManager $entityManager
+     */
+    public function __construct(EntityManager $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    public function apply(Request $request, ParamConverter $configuration)
+    {
+        $decoded = json_decode($request->getContent(), true);
+
+        if (!isset($decoded['article_number']) || !isset($decoded['category'])) {
+            throw new \InvalidArgumentException('Artikel konnte nicht erstellt werden, fehlende Parameter', 400);
+        }
+
+        $article = new Article();
+        $article->setArticleNumber($decoded['article_number']);
+
+        /** @var ArticleCategory $artCat */
+        $artCat = $this->entityManager->find('AppBundle:ArticleCategory', $decoded['category']);
+        if ($artCat !== null) {
+            $article->setCategory($artCat);
+        }
+
+        $request->attributes->set($configuration->getName(), $article);
+
+        return true;
+    }
+
+    public function supports(ParamConverter $configuration)
+    {
+        return $configuration->getClass() === 'AppBundle:Article';
+    }
+}
